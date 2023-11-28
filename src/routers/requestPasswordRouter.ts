749392e1459fc1ef95cede9c033cd2da280413a1
Liseidy Bueno <liseidybueno@gmail.com>
2023-11-27 20:04:26 -0500

    clean up requestPasswordRouter code

--- src/routers/requestPasswordRouter.ts
+++ src/routers/requestPasswordRouter.ts
@@ -3,41 +3,24 @@ import {
   resetPasswordRequestController,
   resetPasswordController,
 } from "../controllers/authController";
-// import { resetPasswordRequestController } from "../controllers/authController";
 
 const requestPasswordRouter: Router = express.Router();
 const resetPasswordRouter: Router = express.Router();
-
-// const router = require("express").Router();
-
-// router.post("/auth/requestResetPassword", resetPasswordRequestController);
 
 requestPasswordRouter.post("/", async (req: Request, res: Response) => {
   try {
     await resetPasswordRequestController(req, res);
   } catch (error) {
-    console.log("****there was an error");
+    console.log("Reset Password Request Controller Error", error);
   }
 });
 
 resetPasswordRouter.post("/", async (req: Request, res: Response) => {
-  console.log("****reset password router");
   try {
-    const didResetPassword = await resetPasswordController(req, res);
-    console.log("***didreset", didResetPassword.req.body);
-    // return didResetPassword.json();
+    await resetPasswordController(req, res);
   } catch (error) {
-    console.log("****error");
+    console.log("Reset Pasword Controller Error", error);
   }
 });
 
 export { requestPasswordRouter, resetPasswordRouter };
-
-// export default requestPasswordRouter.post(
-//   "/",
-//   async (req: Request, res: Response) => {
-//     console.log("****request password router", req);
-//     res.status(200).send(resetPasswordRequestController);
-//   }
-// );
-// router.post("/auth/resetPassword", resetPasswordController);
